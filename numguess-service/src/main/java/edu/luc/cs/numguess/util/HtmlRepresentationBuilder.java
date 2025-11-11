df1107505c239b04895e25446780b7c7f4bc754c
Konstantin Läufer <laufer@cs.luc.edu>
2025-11-10 22:47:06 -0600

    Fix String.format argument order in buildGameActiveHtml
    The action URL placeholder (%s for UUID) and the guesses placeholder (%d) were
    in mismatched order between the template and the String.format() arguments.
    Now correctly passes uuid.toString() first and game.getNumGuesses() second.
    
    This was causing XML parsing errors when browsers tried to render the HTML.

--- numguess-service/src/main/java/edu/luc/cs/numguess/util/HtmlRepresentationBuilder.java
+++ numguess-service/src/main/java/edu/luc/cs/numguess/util/HtmlRepresentationBuilder.java
@@ -278,7 +278,7 @@ public class HtmlRepresentationBuilder {
                 </div>
             </body>
             </html>
-            """, game.getNumGuesses(), uuid);
+            """, uuid.toString(), game.getNumGuesses());
     }
 
     /**
